386dcac18fd49e084418342f852b0666a5c9021a
Ehsan <ehsanmoodi90@gmail.com>
2023-05-26 15:02:29 +0330

    wip => select box

--- components/index-hero-search.tsx
+++ components/index-hero-search.tsx
@@ -1,4 +1,4 @@
-import { SearchNormal } from 'iconsax-react'
+import { FilterSearch, Location, SearchNormal } from 'iconsax-react'
 import { Select, SelectItem } from './select'
 
 export function IndexHeroSearch() {
@@ -7,21 +7,32 @@ export function IndexHeroSearch() {
       <p className="text-base font-normal">
         بهترین کسب و کار و خدمات مورد نیازتان را پیدا کنید
       </p>
-      <div className="flex flex-col gap-4 lg:flex-row">
-        <Select defaultValue="">
+      <div className="flex flex-col gap-4 lg:flex-row lg:items-center">
+        <Select
+          defaultValue=""
+          classes="grow w-full"
+          icon={<Location size={20} className="text-neutral-300" />}
+        >
           <SelectItem value="">محدوده فعالیت</SelectItem>
           <SelectItem value="1">انتخاب یک</SelectItem>
           <SelectItem value="2">انتخاب دو</SelectItem>
           <SelectItem value="3">انتخاب سه</SelectItem>
         </Select>
-        <Select defaultValue="">
+        <Select
+          defaultValue=""
+          classes="grow w-full"
+          icon={<FilterSearch size={20} className="text-neutral-300" />}
+        >
           <SelectItem value="">نوع فعالیت</SelectItem>
           <SelectItem value="1">انتخاب یک</SelectItem>
           <SelectItem value="2">انتخاب دو</SelectItem>
           <SelectItem value="3">انتخاب سه</SelectItem>
         </Select>
-        <button className="inline-flex min-h-[48px] min-w-[48px] items-center justify-center rounded-lg border border-primary bg-primary transition-all">
-          <SearchNormal size={22} className="text-white" />
+        <button className="group inline-flex h-[48px] min-w-[48px] shrink-0 items-center justify-center rounded-lg border border-primary bg-primary transition-all hover:bg-white">
+          <SearchNormal
+            size={22}
+            className="text-white group-hover:text-primary"
+          />
         </button>
       </div>
     </div>
--- components/select.tsx
+++ components/select.tsx
@@ -1,62 +1,52 @@
 'use client'
 
-import { forwardRef, ForwardedRef, ReactNode } from 'react'
+import { ReactNode } from 'react'
 import * as RadinSelect from '@radix-ui/react-select'
-import { ArrowDown2, ArrowUp2, Check } from 'iconsax-react'
+import { ArrowDown2, ArrowUp2, TickCircle } from 'iconsax-react'
 
 interface SelectProps extends RadinSelect.SelectProps {
   classes?: string
   icon?: ReactNode
 }
 
-export const Select = forwardRef<
-  HTMLDivElement,
-  React.ComponentProps<typeof RadinSelect.Root>
->(function Select(
-  { children, icon, classes, ...props }: SelectProps,
-  forwardedRef
-) {
+export function Select({ children, icon, classes, ...props }: SelectProps) {
   return (
     <RadinSelect.Root {...props}>
       <RadinSelect.Trigger
-        ref={forwardedRef as ForwardedRef<HTMLButtonElement>}
-        className={`border border-[red]`}
+        className={`flex flex-row-reverse items-center gap-2 rounded-lg border border-transparent bg-neutral-100 p-[14px] outline-0 transition-all focus:border-primary ${classes}`}
       >
         {icon}
         <RadinSelect.Value />
-        <RadinSelect.Icon>
+        <RadinSelect.Icon className="me-auto">
           <ArrowDown2 size={16} className="text-neutral-300" />
         </RadinSelect.Icon>
       </RadinSelect.Trigger>
       <RadinSelect.Portal>
-        <RadinSelect.Content>
-          <RadinSelect.ScrollUpButton>
-            <ArrowUp2 />
-          </RadinSelect.ScrollUpButton>
+        <RadinSelect.Content
+          position="popper"
+          align="center"
+          sideOffset={10}
+          className="w-full overflow-hidden rounded-lg border border-[red] bg-white p-1 shadow-md"
+        >
           <RadinSelect.Viewport>{children}</RadinSelect.Viewport>
-          <RadinSelect.ScrollDownButton>
-            <ArrowDown2 />
-          </RadinSelect.ScrollDownButton>
         </RadinSelect.Content>
       </RadinSelect.Portal>
     </RadinSelect.Root>
   )
-})
+}
 
-Select.displayName = 'Select'
+interface SelectItemProps extends RadinSelect.SelectItemProps {}
 
-export const SelectItem = forwardRef<
-  HTMLDivElement,
-  React.ComponentProps<typeof RadinSelect.Item>
->(function SelectItem({ children, ...props }, forwardedRef) {
+export function SelectItem({ children, ...props }: SelectItemProps) {
   return (
-    <RadinSelect.Item {...props} ref={forwardedRef}>
+    <RadinSelect.Item
+      {...props}
+      className="flex w-full flex-row-reverse items-center justify-between"
+    >
       <RadinSelect.ItemText>{children}</RadinSelect.ItemText>
       <RadinSelect.ItemIndicator>
-        <Check />
+        <TickCircle size={20} className="text-primary" />
       </RadinSelect.ItemIndicator>
     </RadinSelect.Item>
   )
-})
-
-SelectItem.displayName = 'SelectItem'
+}
